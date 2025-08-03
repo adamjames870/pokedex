598faa572529e8fcf1f118ae4c5d2d5e61dd7fd5
adamjames870 <adamjames870@yahoo.co.uk>
2025-08-03 14:09:27 +0800

    Adds commands in a map and struct

--- main.go
+++ main.go
@@ -7,7 +7,16 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
+
+	commands := getCommands()
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 
@@ -26,11 +35,49 @@ func main() {
 		words := cleanInput(input)
 		firstWord := words[0]
 
-		fmt.Println("Your command was:", firstWord)
+		cmd, exists := commands[firstWord]
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
+		err := cmd.callback()
+		if err != nil {
+			fmt.Printf("Error: %s", err)
+		}
 
 	}
 }
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	commands := getCommands()
+	fmt.Println("Welcome to the Pokedex!")
+	for _, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
